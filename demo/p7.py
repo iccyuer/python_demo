0eefeab0e835f500978660caa32fe40e5dffe15d
yankuan <yankuan@kongzhong.com>
2020-03-19 23:09:17 +0800

    .

--- demo/p7.py
+++ demo/p7.py
@@ -7,6 +7,8 @@ def level_a(s):
     return 85<=s<=100
 
 students=pd.read_excel('./Students.xlsx',index_col='ID')
+
+# 筛选 loc 类似于sql where
 students=students.loc[students['Age'].apply(age_18_to_30)].loc[students['Score'].apply(level_a)]
 
 print(students)
