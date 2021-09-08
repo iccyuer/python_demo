72f4e04c01dc348e1282f54440a4dbec994b28de
yankuan <yankuan@kongzhong.com>
2021-09-08 18:42:26 +0800

    a

---
+++ bin/d1.py
@@ -0,0 +1,7 @@
+# from lib import PeoPle
+from ut import PeoPle
+
+
+if __name__ == '__main__':
+    p1 = PeoPle()
+    p1.go(2)
---
+++ ut/People.py
@@ -0,0 +1,6 @@
+class People:
+    def __init__(self):
+        print('init')
+
+    def go(self,amount):
+        print('go',amount)
