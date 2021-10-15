ea3bf5bf727d4c3b01d380642b6a7ab3f19102d0
yankuan <yankuan@kongzhong.com>
2021-10-15 17:52:34 +0800

    i

--- bin/d1.py
+++ bin/d1.py
@@ -1,4 +1,4 @@
-# from lib import PeoPle
+from lib import PeoPle
 # from ut import PeoPle
 
 # import sys
@@ -6,10 +6,10 @@
 
 # from lib.DateUtil import DateUtil
 
-from ut.DateUtil import DateUtil
+# from ut.DateUtil import DateUtil
 
 if __name__ == '__main__':
-    print(DateUtil().get_today())
-    pass
-    # p1 = PeoPle()
-    # p1.go(2)
+    # print(DateUtil().get_today())
+    # pass
+    p1 = People.PeoPle()
+    p1.go(2)
