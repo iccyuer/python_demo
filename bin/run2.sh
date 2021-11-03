a5656907102d2437aeaa0cb3da929c94392efbd2
yankuan <yankuan@kongzhong.com>
2021-11-03 15:17:51 +0800

    update

---
+++ bin/run2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /root/code/python_demo/bin
+source /root/code/python_demo/venv/bin/activate
+
+# nohup python3 trader_t.py > ../logs/trader_t.log 2>&1 &
+python3 trader_t.py > ../logs/trader_t.log 2>&1 &
