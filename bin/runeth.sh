72f46b0ff267c6ba7fa6903186f44894b7ea9d1b
yankuan <yankuan@kongzhong.com>
2021-11-25 21:19:04 +0800

    i

--- bin/runeth.sh
+++ bin/runeth.sh
@@ -4,4 +4,4 @@ cd /root/code/python_demo/bin
 source /root/code/python_demo/venv/bin/activate
 
 # nohup python3 trader_t.py > ../logs/trader_t.log 2>&1 &
-python3 trader_tdown.py > ../logs/trader_tdown.log 2>&1 &
+python3 trader_eth.py > ../logs/trader_eth.log 2>&1 &
--- bin/runethdown.sh
+++ bin/runethdown.sh
@@ -4,4 +4,4 @@ cd /root/code/python_demo/bin
 source /root/code/python_demo/venv/bin/activate
 
 # nohup python3 trader_t.py > ../logs/trader_t.log 2>&1 &
-python3 trader_t.py > ../logs/trader_t.log 2>&1 &
+python3 trader_ethdown.py > ../logs/trader_ethdown.log 2>&1 &
